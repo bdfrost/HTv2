87c64da88a84efa70022428a5f2a2338e5c777a0
brianfrost <brian.frost@wendys.com>
2024-05-06 19:46:09 -0500

    small cleanup mod

--- .commands/cleanup.sh
+++ .commands/cleanup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 df -h
 sudo find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <( sudo docker ps -aq | xargs sudo docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)' ) | sudo xargs -r sudo rm -fr
 sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r sudo docker rm -v
